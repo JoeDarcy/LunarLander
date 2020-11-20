a6de845dc1bd73bd460e42de176f12fcd87ed3b9
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2020-11-20 17:44:25 +0000

    Player movement inputs and screen buffer clearing
    Taking the player's inputs, (W, A, S and D) and using them to contrtol a characters position on the screen. Also clearing the screen buffer before working out the player's position so as to not leave a trail of chars behind the player char as it moves.

--- LunarLander/LunarLander/Main.cpp
+++ LunarLander/LunarLander/Main.cpp
@@ -26,42 +26,77 @@ HANDLE wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
 HANDLE rHnd = GetStdHandle(STD_INPUT_HANDLE);
 #pragma endregion
 
-// GAME
-const unsigned char characterToPrint = 219;
+// GAME CONSTANTS
+const int playerCharacter = '@';
+const int playerColour = 7;
 
+const int KeyEsc = VK_ESCAPE;
+const int KeyW = 'W';
+const int KeyA = 'A';
+const int KeyS = 'S';
+const int KeyD = 'D';
+
+
+// GAME VARIABLES
+int playerXPos = 0;
+int playerYPos = 0;
 
 int main()
 {
 	// Set the console title
 	SetConsoleTitle(L"Title of my Console Window");
 
-	//Set screen buffer size
+	// Set screen buffer size
 	SetConsoleScreenBufferSize(wHnd, bufferSize);
 
-	// Set up the window
+	// Set up the window size
 	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
 
 	// Bool for exiting the main game loop
 	bool exitGame = false;
 
-	// Display characters to the console window
+	// Display characters to the console window, (main game loop)
 	while (!exitGame)
 	{
-		if (GetAsyncKeyState(VK_ESCAPE))
+		if (GetAsyncKeyState(KeyEsc))	// Checks if player presses "Escape", quits game if pressed
 		{
 			exitGame = true;
 		}
-
-		for (int y = 0; y < HEIGHT; y++)
+		if (GetAsyncKeyState(KeyW))		// Checks if player presses "W", moves character up if pressed
 		{
-			for (int x = 0; x < WIDTH; x++)
-			{
-				consoleBuffer[x + WIDTH * y].Char.AsciiChar = characterToPrint; // Chooses character to print
-				consoleBuffer[x + WIDTH * y].Attributes = rand() % 256;			// Choose random colour
-			}
+			--playerYPos;
+		}
+		if (GetAsyncKeyState(KeyA))	// Checks if player presses "A", moves character left if pressed
+		{
+			--playerXPos;
+		}
+		if (GetAsyncKeyState(KeyS))	// Checks if player presses "S", moves character down if pressed
+		{
+			++playerYPos;
+		}
+		if (GetAsyncKeyState(KeyD))	// Checks if player presses "D", moves character right if pressed
+		{
+			++playerXPos;
 		}
 
-		// Draw the console buffer array to the screen
+		// Clear the previous "frame" before we start to build the next one
+		for (int i = 0; i < (WIDTH * HEIGHT); i++)
+		{
+			consoleBuffer[i].Char.AsciiChar = 0;
+			consoleBuffer[i].Attributes = 0;
+		}
+
+		// Get character struct
+		CHAR_INFO thisChar = consoleBuffer[playerXPos + WIDTH * playerYPos];
+
+		//Set the ASCII character and colour for the player
+		thisChar.Char.AsciiChar = playerCharacter;
+		thisChar.Attributes = playerColour; 
+
+		// Override that character in our buffer with new data
+		consoleBuffer[playerXPos + WIDTH * playerYPos] = thisChar;
+
+		// Draw the console buffer array to the screen, (rendering to the screen)
 		WriteConsoleOutputA(wHnd, consoleBuffer, characterBufferSize, characterPosition, &consoleWriteArea);
 
 	}
